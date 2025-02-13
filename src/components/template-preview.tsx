eaa3c7f7e0545674d12f2449b060f52915518b7f
LORDPRITHWISH <prithwishchakraborty05@gmail.com>
2025-02-13 15:57:51 +0530

    temp

--- src/components/template-preview.tsx
+++ src/components/template-preview.tsx
@@ -1,51 +1,42 @@
 "use client";
 
+import Iphone15Pro from "./magicui/iphone-15-pro";
 // import Image from "next/image";
 // import { Button } from "@/components/ui/button";
 // import { Card } from "@/components/ui/card";
-import PreWindow from "./PreWindow";
+// import PreWindow from "./PreWindow";
 import Temp1 from "./template/Temp1";
 
+// const classname = "px-1 overflow-hidden h-[300] w-[300] md:h-[600] md:w-[300] translate-x-7 md:translate-x-0";
+const messages = [
+  `Welcome,bro 👑`,
+  `Your presence brightens my day...`,
+  `Every moment is magical ✨`,
+  `You're absolutely incredible 🌟`,
+  `Together, we create perfection 🎵`,
+  `Will you be my Valentine? 🌹`,
+];
 
-  // const classname = "px-1 overflow-hidden h-[300] w-[300] md:h-[600] md:w-[300] translate-x-7 md:translate-x-0";
-  const messages = [
-    `Welcome,bro 👑`,
-    `Your presence brightens my day...`,
-    `Every moment is magical ✨`,
-    `You're absolutely incredible 🌟`,
-    `Together, we create perfection 🎵`,
-    `Will you be my Valentine? 🌹`,
-  ];
+const moods = ["superHappy", "excited", "happy", "hopeful", "nervous", "question"];
 
-  const moods = [
-    "superHappy",
-    "excited",
-    "happy",
-    "hopeful",
-    "nervous",
-    "question",
-  ];
-
-  const noButtonMessages = [
-    "No",
-    "Are you sure?",
-    "Really sure?",
-    "Think again!",
-    "Last chance!",
-    "Surely not?",
-    "You might regret this!",
-    "Give it another thought!",
-    "Are you absolutely certain?",
-    "This could be a mistake!",
-    "Have a heart!",
-    "Don't be so cold!",
-    "Change of heart?",
-    "Wouldn't you reconsider?",
-  ];
-  const celebrationMediaUrl =
-    "https://media.giphy.com/media/3o7TKz9bX9v9KzCnXK/giphy.gif";
-  const celebrationMessage =
-    "Congratulations! You have reached the end of this page! 🎉";
+const noButtonMessages = [
+  "No",
+  "Are you sure?",
+  "Really sure?",
+  "Think again!",
+  "Last chance!",
+  "Surely not?",
+  "You might regret this!",
+  "Give it another thought!",
+  "Are you absolutely certain?",
+  "This could be a mistake!",
+  "Have a heart!",
+  "Don't be so cold!",
+  "Change of heart?",
+  "Wouldn't you reconsider?",
+];
+const celebrationMediaUrl = "https://media.giphy.com/media/3o7TKz9bX9v9KzCnXK/giphy.gif";
+const celebrationMessage = "Congratulations! You have reached the end of this page! 🎉";
 
 export function TemplatePreview({ template }) {
   return (
@@ -55,8 +46,8 @@ export function TemplatePreview({ template }) {
       <div className="h-[46px] w-[3px] bg-gray-800 absolute -left-[17px] top-[178px] rounded-l-lg overflow-hidden z-60"></div>
       <div className="h-[64px] w-[3px] bg-gray-800 absolute -right-[17px] top-[142px] rounded-r-lg overflow-hidden z-60"></div>
       <div className="rounded-[2rem] overflow-hidden w-[272px] h-[572px] bg-white">
-        <div className="overflow-hidden w-full h-fit overflow-y-auto py-4z bg-pink-500 z-20">
-          
+        <div className="overflow-hidden w-full overflow-y-auto py-4z bg-pink-500 z-20">
+
             <Temp1
               messages={messages}
               moods={moods}
@@ -65,9 +56,28 @@ export function TemplatePreview({ template }) {
               celebrationMediaUrl={celebrationMediaUrl}
               celebrationMessage={celebrationMessage}
             />
-          
+
         </div>
       </div>
     </div>
   );
 }
+
+// import Iphone15Pro from "@/registry/magicui/iphone-15-pro";
+
+// export function TemplatePreview() {
+//   return (
+//     <div className="relative">
+//       <Iphone15Pro className="size-full">
+//         <Temp1
+//           messages={messages}
+//           moods={moods}
+//           prev={true}
+//           noButtonMessages={noButtonMessages}
+//           celebrationMediaUrl={celebrationMediaUrl}
+//           celebrationMessage={celebrationMessage}
+//         />
+//       </Iphone15Pro>
+//     </div>
+//   );
+// }
