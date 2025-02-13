0e68104678261087c475b1a29715ff082ea5a7cc
LORDPRITHWISH <prithwishchakraborty05@gmail.com>
2025-02-13 20:02:18 +0530

    nav

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className="shadow-md mb-4">
+      <header className="p-4 flex justify-between items-center w-full md:w-[90%] mx-auto texture mb-4">
+        <h1 className="text-2xl font-bold text-pink-600">Will you be my valentine?</h1>
+        <nav className="space-x-4">
+          <Link href="/" className="text-pink-600 hover:text-pink-700">
+            Home
+          </Link>
+          <Link href="/about" className="text-pink-600 hover:text-pink-700">
+            About
+          </Link>
+          <Link href="/contact" className="text-pink-600 hover:text-pink-700">
+            Contact
+          </Link>
+        </nav>
+      </header>
+      <hr className="border-t border-pink-400" />
+    </div>
+  );
+}
